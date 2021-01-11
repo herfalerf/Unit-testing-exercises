c0cd287d94b537f06ef9da04062148c24bfac3cb
unknown <jpendy@gmail.com>
2021-01-11 00:18:32 -0600

    test commit

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,10 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it('should calculate the monthly rate correctly', function() {
+	// ...
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it('should return a result with 2 decimal places', function() {
+	// ..
 });
 
 /// etc
+///booger
